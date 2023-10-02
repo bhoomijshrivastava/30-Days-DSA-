055e329ca63f8c0a47c543790498df2d593a3eba
bhoomijshrivastava <128286940+bhoomijshrivastava@users.noreply.github.com>
2023-10-02 23:31:06 +0530

    Add files via upload

---
+++ Day 2/ReverseArray.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   int arr[] = {10, 20, 30, 40, 50};
+   int n = sizeof(arr)/sizeof(arr[0]);
+
+   for(int i=n-1; i>=0; i--)
+      cout<<arr[i]<<" ";
+}
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
